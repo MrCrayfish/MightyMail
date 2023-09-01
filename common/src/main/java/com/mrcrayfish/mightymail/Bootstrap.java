0f512229a81a211d11557455a1d0e5d357fdd7fa
MrCrayfish <4958241+MrCrayfish@users.noreply.github.com>
2023-09-01 21:42:16 +0930

    🐛 Fixed mailboxes not sent to clients in specific scenario

--- common/src/main/java/com/mrcrayfish/mightymail/Bootstrap.java
+++ common/src/main/java/com/mrcrayfish/mightymail/Bootstrap.java
@@ -1,11 +1,13 @@
 package com.mrcrayfish.mightymail;
 
+import com.mrcrayfish.framework.api.event.PlayerEvents;
 import com.mrcrayfish.framework.api.event.TickEvents;
 import com.mrcrayfish.mightymail.core.ModItems;
 import com.mrcrayfish.mightymail.item.PackageItem;
 import com.mrcrayfish.mightymail.mail.DeliveryService;
 import com.mrcrayfish.mightymail.network.Network;
 import net.minecraft.core.Direction;
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.Containers;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.DispenserBlock;
@@ -20,6 +22,11 @@ public class Bootstrap
     {
         Network.init();
         TickEvents.START_SERVER.register(server -> DeliveryService.get(server).ifPresent(DeliveryService::serverTick));
+        PlayerEvents.LOGGED_OUT.register(player -> {
+            if(player instanceof ServerPlayer serverPlayer) {
+                DeliveryService.get(serverPlayer.server).ifPresent(service -> service.playerLoggedOut(serverPlayer));
+            }
+        });
         DispenserBlock.registerBehavior(ModItems.PACKAGE::get, (source, stack) -> {
             Direction direction = source.getBlockState().getValue(DispenserBlock.FACING);
             Vec3 pos = source.getPos().relative(direction).getCenter();
