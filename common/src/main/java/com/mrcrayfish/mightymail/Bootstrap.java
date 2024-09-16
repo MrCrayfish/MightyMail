3ea3cec2099e499e4e38e1d572ca21c4e24907f4
MrCrayfish <4958241+MrCrayfish@users.noreply.github.com>
2024-09-16 11:53:48 +0930

    🐛 Fix crash when launching without refurbished furniture

--- common/src/main/java/com/mrcrayfish/mightymail/Bootstrap.java
+++ common/src/main/java/com/mrcrayfish/mightymail/Bootstrap.java
@@ -1,15 +1,14 @@
 package com.mrcrayfish.mightymail;
 
-import com.mrcrayfish.framework.api.event.PlayerEvents;
 import com.mrcrayfish.framework.api.event.ServerEvents;
 import com.mrcrayfish.framework.api.event.TickEvents;
+import com.mrcrayfish.framework.platform.Services;
 import com.mrcrayfish.mightymail.command.MigrateCommand;
 import com.mrcrayfish.mightymail.core.ModItems;
 import com.mrcrayfish.mightymail.item.PackageItem;
 import com.mrcrayfish.mightymail.mail.DeliveryService;
 import com.mrcrayfish.mightymail.network.Network;
 import net.minecraft.core.Direction;
-import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.Containers;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.DispenserBlock;
@@ -25,7 +24,10 @@ public class Bootstrap
         Network.init();
         TickEvents.START_SERVER.register(server -> DeliveryService.get(server).ifPresent(DeliveryService::serverTick));
         ServerEvents.STARTING.register(server -> {
-            MigrateCommand.register(server.getCommands().getDispatcher());
+            // Only register if furniture mod is installed
+            if(Services.PLATFORM.isModLoaded("refurbished_furniture")) {
+                MigrateCommand.register(server.getCommands().getDispatcher());
+            }
         });
         DispenserBlock.registerBehavior(ModItems.PACKAGE::get, (source, stack) -> {
             Direction direction = source.getBlockState().getValue(DispenserBlock.FACING);
